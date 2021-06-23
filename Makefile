bea22aa02a4876f99af3344423c9bd948fc29951
Jeff Goeders <jeff.goeders@gmail.com>
2021-06-22 18:18:17 -0600

    Working on first draft
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- Makefile
+++ Makefile
@@ -1,17 +1,30 @@
 BUILD_DIR = build
 PDF_NAME = cv
 
+BUILD_PY = build.py
+DATA_YML = cv_data.yml
 TEMPLATE_TEX = template/cv.tex
 RENDERED_TEX = build/rendered.tex
 
+IN_ENV = if [ -e .venv/bin/activate ]; then . .venv/bin/activate; fi;
 
 $(PDF_NAME).pdf: $(RENDERED_TEX)
-	cd $(BUILD_DIR) && pdflatex -jobname=$(PDF_NAME) ../$^ 
+	cd $(BUILD_DIR) && pdflatex -jobname=$(PDF_NAME) -output-directory=. ../$^ 
 	cp $(BUILD_DIR)/$(PDF_NAME).pdf .
 
-$(RENDERED_TEX): $(BUILD_DIR)
-	python3 build.py $(TEMPLATE_TEX) > $@
+$(RENDERED_TEX): $(BUILD_DIR) $(TEMPLATE_TEX) $(DATA_YML) $(BUILD_PY) Makefile
+	$(IN_ENV) python3 $(BUILD_PY) $(TEMPLATE_TEX) $(DATA_YML) > $@
 
 $(BUILD_DIR):
 	mkdir $@
 	
+env:
+	python3.8 -m venv .venv
+	$(IN_ENV) pip install --upgrade pip
+	$(IN_ENV) pip install nbconvert pypandoc pyyaml
+
+packages:
+	sudo apt-get install -y pandoc
+
+clean:
+	rm -rf $(BUILD_DIR) cv.pdf
---
+++ build.py
@@ -0,0 +1,47 @@
+import jinja2
+import os
+from jinja2 import Template
+import argparse
+import pathlib
+import yaml
+from nbconvert.filters import markdown2latex, escape_latex
+
+
+def bold_my_name(s):
+    if s:
+        return s.replace("Jeffrey Goeders", r"\textbf{Jeffrey Goeders}")
+    else:
+        return s
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("template_path", type=pathlib.Path)
+    parser.add_argument("data_yml_path", type=pathlib.Path)
+    args = parser.parse_args()
+
+    env = jinja2.Environment(
+        block_start_string="\BLOCK{",
+        block_end_string="}",
+        variable_start_string="\VAR{",
+        variable_end_string="}",
+        comment_start_string="\#{",
+        comment_end_string="}",
+        # line_statement_prefix="%%",
+        # line_comment_prefix="%#",
+        trim_blocks=True,
+        autoescape=False,
+        loader=jinja2.FileSystemLoader(os.path.abspath(".")),
+    )
+    env.filters["escape_latex"] = escape_latex
+    env.filters["md2latex"] = markdown2latex
+    env.globals["bold_my_name"] = bold_my_name
+
+    template = env.get_template(str(args.template_path))
+    with open(args.data_yml_path) as fp:
+        data = yaml.safe_load(fp)
+    print(template.render(data))
+
+
+if __name__ == "__main__":
+    main()
